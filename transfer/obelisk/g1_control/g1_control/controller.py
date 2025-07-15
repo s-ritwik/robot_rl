1eed84f5d14ae24e41e98f806b18e4efecc026e2
Amy <kli5@caltech.edu>
2025-07-15 10:53:24 -0700

    added gait library version of the observation for hardware transfo

--- transfer/obelisk/g1_control/g1_control/controller.py
+++ transfer/obelisk/g1_control/g1_control/controller.py
@@ -29,7 +29,9 @@ class VelocityTrackingController(ObeliskController, ABC):
         # Load policy
         self.declare_parameter("policy_name", "")
         self.declare_parameter("height_map_flag", False)
+        self.declare_parameter("gl_flag", False)
         self.height_map_flag = self.get_parameter("height_map_flag").get_parameter_value().bool_value
+        self.gl_flag = self.get_parameter("gl_flag").get_parameter_value().bool_value
         policy_name = self.get_parameter("policy_name").get_parameter_value().string_value
         pkg_path = get_package_share_directory("g1_control")
         policy_path = os.path.join(pkg_path, f"resource/policies/{policy_name}")
@@ -287,6 +289,35 @@ class VelocityTrackingController(ObeliskController, ABC):
 
         return obs
 
+    def get_gl_obs(self):
+      
+        obs = np.zeros(self.num_obs, dtype=np.float32)
+
+        obs[:3] = self.omega * self.ang_vel_scale                                                 # Angular velocity
+        obs[3:6] = self.proj_g                                        # Projected gravity
+        obs[6] = self.cmd_vel[0]*self.cmd_scale[0]                                   # Command velocity
+        obs[7] = self.cmd_vel[1]*self.cmd_scale[1]                                   # Command velocity
+        obs[8] = self.cmd_vel[2]*self.cmd_scale[2]     
+                                     # Command velocity
+
+        joint_pos_isaac = self._convert_to_isaac(self.joint_pos, self.joint_names) - self.default_angles_isaac
+        nj = len(joint_pos_isaac)
+
+        joint_vel_isaac = self._convert_to_isaac(self.joint_vel, self.joint_names)
+        obs[9 : 9 + nj] = joint_vel_isaac* self.qvel_scale  # Joint vel
+        obs[9 + nj : 9 + 2 * nj] =  joint_pos_isaac
+     
+
+        obs[9 + 2 * nj : 9 + 3 * nj] = self.action  # Past action
+
+        sin_phase = np.sin(2 * np.pi * self.time / self.period)
+        cos_phase = np.cos(2 * np.pi * self.time / self.period)
+
+        obs[9 + 3 * nj : 9 + 3 * nj + 2] = np.array([sin_phase, cos_phase])  # Phases
+
+        obs_tensor = torch.from_numpy(obs).unsqueeze(0).float()
+        return obs_tensor
+
     def get_cnn_obs(self):
         """Create the observation vector from the sensor data"""
         # height_obs = self.convert_height_map_to_obs(height_map, sensor_pos)
@@ -333,6 +364,8 @@ class VelocityTrackingController(ObeliskController, ABC):
         if self.received_xhat:
             if self.height_map_flag:
                 obs = self.get_cnn_obs()
+            elif self.gl_flag:
+                obs = self.get_gl_obs()
             else:
                 obs = self.get_obs()
 
