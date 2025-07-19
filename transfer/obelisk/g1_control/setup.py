49014aa2c968a3c966c36bd4e9b6628226d8696f
Amy <kli5@caltech.edu>
2025-07-19 14:17:49 -0700

    added one new policy

--- transfer/obelisk/g1_control/setup.py
+++ transfer/obelisk/g1_control/setup.py
@@ -5,44 +5,34 @@ from setuptools import find_packages, setup
 
 package_name = "g1_control"
 
-# def package_files(directory, patterns=["**/*"]):
-#     paths = []
-#     for pattern in patterns:
-#         for path in glob(os.path.join(directory, pattern), recursive=True):
-#             if os.path.isfile(path):
-#                 install_path = os.path.join("share", package_name, path.replace(directory + "/", ""))
-#                 paths.append((os.path.dirname(install_path), [path]))
-#     return paths
+def package_files(base_dir: str, package_name: str, patterns=["**/*"]):
+    """Recursively collect all files matching patterns under base_dir."""
+    paths = []
+    for pattern in patterns:
+        for path in glob(os.path.join(base_dir, pattern), recursive=True):
+            if os.path.isfile(path):
+                # Create destination path under share/package_name/...
+                rel_path = os.path.relpath(path, base_dir)
+                install_path = os.path.join("share", package_name, base_dir, os.path.dirname(rel_path))
+                paths.append((install_path, [path]))
+    return paths
 
 
+data_files = [
+    ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+    ("share/" + package_name, ["package.xml"]),
+    (os.path.join("share", package_name, "config"), glob(os.path.join("config", "*.yml"))),
+    (os.path.join("share", package_name, "config"), glob(os.path.join("config", "*.obk"))),
+]
 
+# Add all model files recursively under resource/policies
+data_files += package_files("resource/policies", package_name, patterns=["**/*.pt"])
 
 setup(
     name=package_name,
     version="0.0.0",
     packages=find_packages(exclude=["test"]),
-    data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
-        ("share/" + package_name, ["package.xml"]),
-        (os.path.join("share", package_name, "config"), glob(os.path.join("config", "*.yml"))),
-        (os.path.join("share", package_name, "config"), glob(os.path.join("config", "*.obk"))),
-        (
-            os.path.join("share", package_name, "resource/policies"),
-            glob(os.path.join("resource/policies", "*.pt")),
-        ),  # Get the models..
-        (
-            os.path.join("share", package_name, "resource/policies/hzd_gl_policies_07_17"),
-            glob(os.path.join("resource/policies/hzd_gl_policies_07_17", "*.pt")),
-        ),
-        (
-            os.path.join("share", package_name, "resource/policies/hzd_gl_policies_07_17_night"),
-            glob(os.path.join("resource/policies/hzd_gl_policies_07_17_night", "*.pt")),
-        ),
-        (
-            os.path.join("share", package_name, "resource/policies/hzd_gl_policies_07_18"),
-            glob(os.path.join("resource/policies/hzd_gl_policies_07_18", "*.pt")),
-        ),
-    ],
+    data_files=data_files,
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="zolkin",
